323be66d6a84b230c0a6daf74b8abf29209e2189
cyberlearner13 <arkadeepmukherjee2012@gmail.com>
2021-04-02 16:45:33 +0530

    show hide forms as needed

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 document.querySelector('form').addEventListener('submit', e => {
     e.preventDefault();
+
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
+
     const input = document.querySelector('#message-input');
     const encrypted = btoa(input.value);
 
     const link = document.querySelector('#link-input');
     link.value = `${window.location}#${encrypted}`;
     link.select();
+
+
 })
